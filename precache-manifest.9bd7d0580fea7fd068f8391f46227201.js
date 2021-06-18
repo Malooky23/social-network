9be07c784149ed5bc7c8b999c11586af589b0371
Malek <malooky23@gmail.com>
2021-06-18 23:43:25 +0300

    Updates

---
+++ precache-manifest.9bd7d0580fea7fd068f8391f46227201.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c0461838336c10cd31df",
+    "url": "/social-network/static/css/main.aa31f155.chunk.css"
+  },
+  {
+    "revision": "c0461838336c10cd31df",
+    "url": "/social-network/static/js/main.c0461838.chunk.js"
+  },
+  {
+    "revision": "bf72f4c2a7e7cbf9db64",
+    "url": "/social-network/static/css/1.11f3d5c1.chunk.css"
+  },
+  {
+    "revision": "bf72f4c2a7e7cbf9db64",
+    "url": "/social-network/static/js/1.bf72f4c2.chunk.js"
+  },
+  {
+    "revision": "22e660ce69f7e2ad833f",
+    "url": "/social-network/static/js/runtime~main.22e660ce.js"
+  },
+  {
+    "revision": "a62f02d2cc06ceb00eff4c2a24a9151f",
+    "url": "/social-network/index.html"
+  }
+];
